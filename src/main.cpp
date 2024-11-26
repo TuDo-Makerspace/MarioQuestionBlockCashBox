5200da20641cf055fa8e6f425c92b6f6996a0e80
Patrick Pedersen <ctx.xda@gmail.com>
2024-11-26 03:43:02 +0100

    Updated to work with new board
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
  *
  * Author       : Yunis, Patrick Pedersen
  * Description  : Main code for the coin box
- * 
+ *
  */
 
-#include <PCM.h>
 #include <Arduino.h>
+#include <PCM.h>
 
 #include "sounds.h"
 
@@ -15,7 +15,10 @@
 
 // Pins
 // Sound is on pin 11
-#define SENSOR_PIN 8
+#define SENSOR_PIN A7
+
+// Sensor Parameters
+#define SENSOR_THRESHOLD 512
 
 // Number of coins to trigger sound
 #define POWER_UP 4
@@ -24,8 +27,11 @@
 // Function
 #define WAIT_AFTER_COIN 100 // ms
 
+// Macros
+#define SENSOR_READ() (analogRead(SENSOR_PIN) > SENSOR_THRESHOLD ? HIGH : LOW)
+
 void print_boot_msg() {
-  Serial.println("@@@@/////////////////////////////////////////////////@@@@\n");         
+  Serial.println("@@@@/////////////////////////////////////////////////@@@@\n");
   Serial.println("////,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@\n");
   Serial.println("////,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@\n");
   Serial.println("////,,,@@@@,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@,,,@@@@\n");
@@ -60,22 +66,20 @@ void setup() {
 }
 
 void loop() {
-  static uint8_t last_val = digitalRead(SENSOR_PIN);
-  uint8_t val = digitalRead(SENSOR_PIN);
+  static uint8_t last_val = SENSOR_READ();
+  uint8_t val = SENSOR_READ();
 
-  if(last_val == LOW && val == HIGH) {
+  if (last_val == LOW && val == HIGH) {
 
     Serial.print("Coin detected! Playing: ");
 
     if ((money % POWER_UP) == 0) {
       Serial.println("Power up!");
       startPlayback(powerup, sizeof(powerup));
-    }
-    else if ((money % ONE_UP) == 0) {
+    } else if ((money % ONE_UP) == 0) {
       Serial.println("One up!");
       startPlayback(oneup, sizeof(oneup));
-    }
-    else {
+    } else {
       Serial.println("Coin sound!");
       startPlayback(coin, sizeof(coin));
     }
